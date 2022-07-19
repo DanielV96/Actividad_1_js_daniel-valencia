861768c02a1298cda1fd5dc45619b9340e353388
Daniel Valencia M <105326240+DanielV96@users.noreply.github.com>
2022-07-18 21:11:45 -0500

    Add files via upload

---
+++ Actividad_1_js_daniel_valencia/app.js
@@ -0,0 +1,8 @@
+let name = 'Daniel', 
+lastName = 'Valencia',
+ YearOfBirth = 1996,
+ cityOfBirth = 'Medellín'
+
+console.log(
+`Hola, mi nombre es ${name}, 
+${lastName}, vivo en la ciudad de ${cityOfBirth}, y tengo ${2022-YearOfBirth} años.`)
